fa4ac78050469143550fe36f70995d1396f7b515
Felipe Martins <me@fefas.net>
2016-08-23 10:15:02 -0300

    Fix test

--- tests/doubles/BehavioralPattern/ChainOfResponsibility/FakeRequestHandlerA.php
+++ tests/doubles/BehavioralPattern/ChainOfResponsibility/FakeRequestHandlerA.php
@@ -6,5 +6,10 @@ class FakeRequestHandlerA extends RequestHandler
 {
     public function handle(Request $request): Response
     {
+        if ($request->type() == Request::HANDLEABLE or $request->type() == Request::HANDLEABLE_BY_A) {
+            return new FakeResponse('Request was handled by A');
+        }
+
+        return $this->handleUsingSuccessor($request);
     }
 }
